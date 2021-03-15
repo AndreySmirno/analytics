3e51bf44dc7af025ca6a712e7c4fea8d7c218305
AndreySmirno <ksk8andrey@gmail.com>
2021-03-15 12:30:02 +0300

    update_15_03_2021

--- currency_pick.py
+++ currency_pick.py
@@ -5,8 +5,9 @@ import pandas as pd
 import numpy as np
 import scipy.stats as stats
 import statsmodels.tsa.stattools as adf
+import statsmodels.stats.diagnostic as lb
 from arch import arch_model
-
+import warnings
 cur_d = dt.date.today()
 prev_d = dt.date.today() - dt.timedelta(days=900)
 
@@ -16,6 +17,7 @@ with open('C:/py_projects_and_stuff/venv/config_currency.yaml') as file:
 asset_file = []
 for i in range(len(symbol_list['asset'])):
     asset = yf.download(symbol_list['asset'][i], start=str(prev_d), end=str(cur_d), period='1d')
+    asset['symbol'] = symbol_list['asset'][i]
     asset_file.append(asset)
 
 def reset_i(data):
@@ -61,7 +63,6 @@ def yang_zhang_volatility(data, period=int):
 def stationary_test(data):
     fuller = adf.adfuller(data, autolag='AIC')
     kpss = adf.kpss(data, regression='ct', nlags='auto')
-
     if (fuller[0] < fuller[4]['5%']) and (kpss[0] < kpss[3]['5%']):
         return int(1)
     else:
@@ -72,14 +73,72 @@ def stationary_test(data):
 ##                                                   H1: autocorrelation resids are not independent
 ## In our use-case we assume that residuals are independent and there is no serial autocorrelation in set
 ## this mean that error have not inherited from previous valuation
+## There are 3 storyponts such as: 1) Residual Distribution across JB or SW tests, 2) Q-statistics of LB-test. 
 
-stationarity_res = stationary_test(x)
 
-arch_m = arch_model(x, vol='GARCH', p=1, q=1, dist='Normal')
+def kpss_test(series, **kw):
+    statistic, p_value, n_lags, critical_values = adf.kpss(series, **kw)
+    print(f'KPSS Stat: {statistic}')
+    print(f'p-value: {p_value}')
+    print(f'num_lags: {n_lags}')
+    print(f'Critical values:')
+    for key, value in critical_values.items():
+        print(f'    {key}: {value}')
+    print(f'Result: The series is {"not " if p_value < 0.05 else ""}stationary')
+
+def test_garch(data):
+    arch_m = arch_model((data['x'] * 100), vol='GARCH', p=1, q=1, dist='Normal')
+    garch = arch_m.fit(disp='off')
+    t2 = lb.acorr_ljungbox(garch.resid)[1][0]
+    if t2 >= 0.05:
+        garch_volatility = np.sqrt(garch.params['omega'] + garch.params['alpha[1]'] * garch.resid ** 2 +
+                                   garch.conditional_volatility ** 2 * garch.params['beta[1]'])
+        longterm_volty = np.sqrt(garch.params['omega'] / (1 - garch.params['alpha[1]'] - garch.params['beta[1]']))
+        data['garch_vol'] = garch_volatility/100
+        data['Longterm_Volatility'] = longterm_volty/100
+        return data
+    else:
+        print('One day lag is serially correlated')
+
+
+
+quotes_tmp = []
+
+for i in range(len(asset_file)):
+    quote = asset_file[i].reset_index()[['Date', 'symbol', 'Close']]
+    x = np.log(quote['Close'] / quote['Close'].shift(1))
+    x = reset_i(x.dropna())
+    stationarity_res = stationary_test(x)
+    if stationarity_res == 0:
+        print(f'Symbol is not tationary')
+    else:
+        quote = reset_i(quote[1::]) 
+        quote['x'] = x
+        quote = test_garch(quote)
+        quotes_tmp.append(quote)
+    
+
+
+k = quotes_tmp[0]
+k['res'] = abs(k['x'])
+k[['res', 'garch_vol', 'Longterm_Volatility']].plot()
+
+
+
+
+
+
+
+
+
+
+arch_m = arch_model((quotes_tmp[0]['x']*100), vol='GARCH', p=1, q=1, dist='Normal')
 garch = arch_m.fit(disp='off')
+
 garch_volatility = np.sqrt(garch.params['omega'] + garch.params['alpha[1]'] * garch.resid**2 +
                                              garch.conditional_volatility**2 * garch.params['beta[1]'])
 
+garch_volatility.plot()
 longterm_volty = np.sqrt(garch.params['omega'] / (1-garch.params['alpha[1]']-garch.params['beta[1]']))
 
 volty_df = yz_volatility
